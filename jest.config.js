9c18fabab14cd38d26829433be056861f9eeb8a8
Juliano Sousa - Hermes <juliano.sousa@unemat.br>
2026-05-01 17:58:20 -0400

    test: setup Jest configuration
    - Add jest.config.js
    - Add test and test:coverage npm scripts

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*)',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+};
